6387c5eba683d8f346c7ce51e7e3341ff2c6e7e4
maclive <ahmed_okpa@hotmail.com>
2026-04-30 02:35:11 +0300

    Update languages.server.ts

--- app/languages.server.ts
+++ app/languages.server.ts
@@ -2,11 +2,11 @@
  * True dynamic imports don't seem possible from the CF pages context, so we must keep a reference to the languages being imported
  */
 const languagesServer: { [key: string]: () => any } = {
-  ar: () => import('../public/locales/ar.json'),
-  en: () => import('../public/locales/en.json'),
-  es: () => import('../public/locales/es.json'),
-  pt: () => import('../public/locales/pt.json'),
-  'pt-BR': () => import('../public/locales/pt-BR.json'),
+  ar: () => import('./locales/ar.json'),
+  en: () => import('./locales/en.json'),
+  es: () => import('./locales/es.json'),
+  pt: () => import('./locales/pt.json'),
+  'pt-BR': () => import('./locales/pt-BR.json'),
 };
 
 export function findLanguageJSON(language: string, namespace: string) {
