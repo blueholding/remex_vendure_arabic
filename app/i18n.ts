070a85d37aca53b3fe62309300772bcc990c5b28
maclive <ahmed_okpa@hotmail.com>
2026-04-30 23:36:19 +0300

    Delete app directory

--- app/i18n.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  supportedLngs: ['ar', 'en', 'es', 'pt', 'pt-BR'],
-  fallbackLng: 'ar',
-  // Disabling suspense is recommended
-  react: { useSuspense: false },
-  backend: {
-    loadPath: '../public/locales/{{lng}}/{{ns}}.json',
-  },
-};
